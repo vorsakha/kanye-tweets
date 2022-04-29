ef4fe59371901ec0d66e7d48479fd10245866e85
vorsakha <andreitf.dev@gmail.com>
2022-04-29 16:05:02 -0300

    refactor: format data from api properly

--- lib/controllers/tweets_controller.dart
+++ lib/controllers/tweets_controller.dart
@@ -3,15 +3,31 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 Future main() async {
-  final json = await fetch();
+  List list = [];
 
-  print(json);
+  for (var i = 0; i < 3; i++) {
+    final tweet = await fetch();
+    list.add(tweet);
+  }
+
+  print(list[0].quote);
 }
 
-Future<Map> fetch() async {
+Future<Tweet> fetch() async {
   var url = 'https://api.kanye.rest';
   var response = await http.get(Uri.parse(url));
   var json = jsonDecode(response.body);
 
-  return json;
+  var tweet = Tweet.fromJson(json);
+  return tweet;
+}
+
+class Tweet {
+  final String quote;
+
+  Tweet({required this.quote});
+
+  factory Tweet.fromJson(Map json) {
+    return Tweet(quote: json['quote']);
+  }
 }
